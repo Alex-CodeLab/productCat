1bb441f9315bb6bdfdbf8e61ae45781e0ae8901b
alex <lx@lxer.eu>
2022-11-10 11:12:38 +0100

    init

---
+++ README.md
@@ -0,0 +1,43 @@
+
+
+Proof of Concept using MariaDB Dynamic table.
+
+Results:
+Performance of Filtering & searching in a large dynamic table is decent.
+A typical filter on 1M records takes 20-70ms (90ms max) on a simple development machine, without caching.
+This could be optimized further using caching and possibly by defining some types in the table specs.
+Needs further testing to see how it performs on huge amounts of records and attributes.
+
+Features
+- any amount of attributes can be added to a Product
+- Filters in Django-admin are automatically created based on unique keys/values in the dynamic table.
+- The admin-filters show up in the right column
+- New attributes can be added (in json format)
+
+
+Install:
+- make sure MySQL or MariaDB is correctly set up as DatabaseEngine
+- add the app this app in INSTALLED_APPS
+- run migrations to create the Products table
+
+
+Initial test-data can be generated using
+
+     from random import randint
+     import random
+    
+     for i in range(100000):
+         s = str(i)
+         r = str(randint(1, 20))
+         Product.objects.create(name="name_" + s, attrs={"size": random.choice(['s', 'm', 'l', 'xl']),
+                                                         "color": random.choice(['red', 'blue', 'green', 'white'])})
+
+
+Todo:
+- editing the Product-attributes is not implemented
+- some nice Form-widget to create and edit attributes
+- Check if this could work with Postgres HStore 
+- When new attributes are added, a reload is required to have it show up in the admin-filters
+
+
+![](img/djangoadmin.png)
---
+++ admin.py
@@ -0,0 +1,63 @@
+from typing import Any
+
+from django.contrib import admin
+from django import forms
+from products.models import Product
+import json
+
+
+class ProductAdminForm(forms.ModelForm):
+    attributes = forms.CharField()
+
+    class Meta:
+        model = Product
+        fields = ('name',)
+
+
+def get_all_prod_attributes() -> dict[str, Any]:
+    attrs = Product.objects.values('attrs').distinct()
+    attributes = [attr['attrs'] for attr in attrs]
+    att = {}
+    for a in attributes:
+        for k, v in a.items():
+            if k not in att:
+                att[k] = [v]
+            elif v not in att[k]:
+                att[k].append(v)
+    return att
+
+
+def lookups(self, request, model_admin):
+    return ((value, value) for value in self.all_attributes[self.title])
+
+
+def queryset(self, request, queryset):
+    for value in self.all_attributes[self.title]:
+        if self.value() == value:
+            kwargs = {f'attrs__{self.title}_CHAR': value}
+            return queryset.filter(**kwargs)
+
+
+class AttributesFilter(admin.SimpleListFilter):
+    all_attributes = get_all_prod_attributes()
+
+# dynamically create AdminFilter classes
+filters = [type(f"AttributesFilter{k}", (AttributesFilter,), {
+    'title': k,
+    'parameter_name': k,
+    'lookups': lookups,
+    'queryset': queryset})
+   for k, v in get_all_prod_attributes().items()]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    form = ProductAdminForm
+    list_display = ('name', 'attrs')
+    list_filter = [*filters]
+
+    def save_model(self, request, obj, form, change):
+        obj.attrs = json.loads(form.data.get('attributes'))
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Product, ProductAdmin)
